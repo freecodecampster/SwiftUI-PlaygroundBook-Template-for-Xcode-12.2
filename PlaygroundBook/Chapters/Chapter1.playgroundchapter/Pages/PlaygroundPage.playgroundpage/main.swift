c36eed0e4530799c7bccd3dca3bf6c0220fa13f3
freecodecampster <freecodecampster@users.noreply.github.com>
2021-01-04 23:06:31 +0000

    Added some example code showing how to send a PlaygroundValue taken from WWDC16 Introducing Swift Playgrounds

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
@@ -6,4 +6,26 @@
 //  The Swift file containing the source code edited by the user of this playground book.
 //
 //#-end-hidden-code
-let str = "Hello, playground"
+import PlaygroundSupport
+
+func say(_ message: String) {
+    let page = PlaygroundPage.current
+    if let proxy = page.liveView as? PlaygroundRemoteLiveViewProxy {
+        proxy.send(.string(message))
+    }
+}
+
+say("<#Knock, knock!#>")
+
+let page = PlaygroundPage.current
+let proxy = page.liveView as! PlaygroundRemoteLiveViewProxy
+
+let message: PlaygroundValue = .boolean(true)
+proxy.send(message)
+
+let command: PlaygroundValue
+command = .dictionary([
+    "Command": .string("Echo"),
+    "Message": .string("Hello!")
+    ])
+proxy.send(command)
